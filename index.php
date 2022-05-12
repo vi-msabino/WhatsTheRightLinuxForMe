9773d2c080d7aa9ab231d124806d376df61472d5
vi-msabino <manuel.sabino@hof-university.de>
2022-05-12 08:40:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
     <header> 
         <h1 id="header">What is the best Linux Distribution for me?</h1> 
         <button id="dark" onclick="onClick('button')"><img src="dark-white.png" type="img/png" title="Umstellen auf Dark/White-Mode" height="30px"></img></button>
-    	  <p id="Enter">Enter drücken um den Namen einzugeben!</p>
     </header>
     <?php 
         include("teilnehmer.php");
@@ -22,7 +21,9 @@
 <form id="vn" name ="vona" method="post">
     <main >
         <p id="eingabe"><label>Trage hier deinen Namen ein : </label><br><input id="input" type="text" name="vorname"/></p>
+	  <nav id="enter">Enter drücken um den Namen einzugeben!</nav>
         <div><a href="quiz.html" class="COLOR"><img id="tux" src="Tux.svg.png" type="img/png" width="300"><br><p id="bild1">Zum Starten auf das Bild von Tux klicken</p></a></div>
+
     </main>
 </form>
     <footer>
