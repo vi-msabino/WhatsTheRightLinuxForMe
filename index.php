f11394adc4883e44c5ca8aad2c350253d23613f1
LSchreckenast <105206063+LSchreckenast@users.noreply.github.com>
2022-06-01 10:02:03 +0200

    Update index.php
    Form auf GET umgestellt, css in die Datei gezogen. teilnehmer.php entfernt

--- index.php
+++ index.php
@@ -5,30 +5,34 @@
     <meta name="What is the best Linux Distribution for me?">
     <title>Welcome to our Distro-Chooser</title>
     <link rel="stylesheet" href="general.css">
-    <link rel="stylesheet" href="style.css">
+    <!--link rel="stylesheet" href="style.css"-->
     <script type="text/javascript" src="general.js"></script>
-    <script type="text/ptp" src="teilnehmer.php"></script>
 </head>
-
 <body class="COLOR">
     <header> 
         <h1 id="header">What is the best Linux Distribution for me?</h1> 
         <button id="dark" onclick="onClick('button')"><img src="dark-white.png" type="img/png" title="Umstellen auf Dark/White-Mode" height="30px"></img></button>
     </header>
-    <?php 
-        include("teilnehmer.php");
-    ?>
-<form id="vn" name ="vona" method="post">
-    <main >
-        <p id="eingabe"><label>Trage hier deinen Namen ein : </label><br><input id="input" type="text" name="vorname"/></p>
-	  <nav id="enter">Enter drücken um den Namen einzugeben!</nav>
-        <div><a href="quiz.html" class="COLOR"><img id="tux" src="Tux.svg.png" type="img/png" width="300"><br><p id="bild1">Zum Starten auf das Bild von Tux klicken</p></a></div>
-
-    </main>
-</form>
+    <form method="GET" action="quiz_01.php">
+        <label>Trage hier deinen Namen ein : </label><br>
+        <input id="input" type="text" name="vorname"/>
+	    <nav id="enter">Enter drücken um den Namen einzugeben!</nav>
+        <input type="image" src="Tux.svg.png" width="300"/><br>
+        <p id="bild1">Zum Starten auf das Bild von Tux klicken</p>
+    </form>
     <footer>
         <p>© Sabino, Schreckenast</p>
     </footer>
-    </body>
-
+    <style type="text/css">
+        form{
+            position:absolute;
+            left: 50%;
+            top: 10%;
+            height: 80%;
+            width: 100%;
+            text-align: center;
+            transform: translate(-50%, 0%);
+        }
+    </style>
+</body>
 </html>
