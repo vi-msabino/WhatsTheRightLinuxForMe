6a53d46a4129e6d7094975e44c53ea73820162c6
LSchreckenast <105206063+LSchreckenast@users.noreply.github.com>
2022-06-07 17:07:04 +0200

    Update ergebnis.php
    Inserting the input in the database finally works

--- ergebnis.php
+++ ergebnis.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT & ~E_NOTICE);
+
+ini_set('display_errors', '1');
 function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
@@ -23,10 +26,21 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+//naechste id für primärschlüssel suchen
+$prev_id = '1000';
+$result = $conn->query("SELECT max(n_id) FROM Nutzer;");
 
+if ($result->num_rows > 0) { //wenn irgendwas zurückgeliefert wird
+  while($row = $result->fetch_assoc()) { //while Schleife, weil bei mir die mysql_fetch_assoc Variante nicht mag
+    $prev_id = $row["max(n_id)"]; //die aktuell hoechste ID speichern
+  }
+} else {
+  die("Coudnt query Nutzer");
+}
 // set parameters and execute
 // name wird nicht getestet, weil das schon in der quiz.php passiert ist, beim Schreiben in die session
 $name = $_SESSION["name"];
+$curr_id = $prev_id +1;
 $hw_anforderungen = test_input($_GET["hw_anforderungen"]);
 $erfahrungsgrad = test_input($_GET["erfahrungsgrad"]);
 $konfigurierbarkeit = test_input($_GET["konfigurierbarkeit"]);
@@ -35,13 +49,28 @@ $secure_boot = test_input($_GET["secure_boot"]);
 $packetmanager = test_input($_GET["packetmanager"]);
 $quelloffen = test_input($_GET["quelloffen"]);
 
-$sql = "INSERT INTO Nutzer (n_name, n_hw_anforderungen, n_erfahrungsgrad, n_konfigurierbarkeit, n_aktualisierungen, n_secure_boot, n_packetmanager, n_quelloffen)
-VALUES ('$name', $hw_anforderungen, $erfahrungsgrad, $konfigurierbarkeit ,$aktualisierungen , $secure_boot,$packetmanager  , $quelloffen );";
-echo $sql;
-if ($conn->query($sql) === TRUE) {
+$basic_insert = "INSERT INTO Nutzer (n_id, n_name, n_hw_anforderungen, n_erfahrungsgrad, n_konfigurierbarkeit, n_aktualisierungen, n_secure_boot, n_packetmanager, n_quelloffen)
+VALUES ($curr_id, '$name', $hw_anforderungen, $erfahrungsgrad, $konfigurierbarkeit ,$aktualisierungen , $secure_boot,$packetmanager  , $quelloffen );";
+echo $basic_insert;
+if ($conn->query($basic_insert) === TRUE) {
     echo "New record created successfully";
   } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: " . $basic_insert . "<br>" . $conn->error;
   }
+
+
+$i = 0;
+$insert_n_desk = "";
+while(isset($_GET["desktop"][$i])){
+  $curr_desk = test_input($_GET["desktop"][$i]);
+  echo $curr_desk;
+  $i++;
+  $insert_n_desk .= "INSERT INTO Nutzer_Desktop (n_id, d_name) VALUES ($curr_id, '$curr_desk');";
+}
+if (mysqli_multi_query($conn, $insert_n_desk)) {
+  echo "New records created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
 $conn->close();
 ?> 
